4f9a1241bbbc4a0316dbf0499bbf57eaaabce229
jlrmonteagudo <jlrdguez2005@gmail.com>
2018-03-23 14:37:09 -0400

    Add dir test.
    Initial step to test each function with its correct output from 4clojure.com

---
+++ test/clojure_training/core_test.clj
@@ -0,0 +1,9 @@
+(ns clojure-training.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-training.core :refer :all]))
+
+(deftest test-problems
+  (testing "Testin if 0=0 :-||"
+    (is (= 0 0))))            
+
+
